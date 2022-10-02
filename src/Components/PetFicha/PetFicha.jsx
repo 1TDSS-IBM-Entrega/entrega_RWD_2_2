cb2a5ef961fb844df8f2b11c94352c00ba979159
FilipeJava <RM93042@fiap.com.br>
2022-10-02 15:43:41 -0300

    PetFicha criado

--- src/Components/PetFicha/PetFicha.jsx
+++ src/Components/PetFicha/PetFicha.jsx
@@ -1,1 +1,23 @@
 //preparando Pet ficha
+
+import { Div, H2, Button, IMG, P,SPAN, SPAN2 } from "./styled"
+
+export const PetFicha = ({id, name, age, size, breed, tutorName, tutorPhone, petImage, observations, onDelete}) => {
+    console.log(id)
+    return (
+        <>       
+            <Div>
+                <P>Nome do Pet <SPAN>{name}</SPAN></P>
+                <P>Idade do Pet <SPAN2>{age}</SPAN2> </P>
+                <P>Tamanho do Pet <SPAN2>{size}</SPAN2></P>
+                <P>Raça do Pet <SPAN2>{breed}</SPAN2></P>
+                <P>Nome do Dono <SPAN2>{tutorName}</SPAN2></P>
+                <P>Telefone do Dono <SPAN2>{tutorPhone}</SPAN2></P>
+                <IMG src={petImage} alt="imagem do pet" />
+                <P>Observações <SPAN2>{observations}</SPAN2> </P>
+                {/* button delete */}
+                <Button type="button" onClick={() => onDelete(id)}>Delete</Button>
+            </Div>
+        </>    
+    )
+}
