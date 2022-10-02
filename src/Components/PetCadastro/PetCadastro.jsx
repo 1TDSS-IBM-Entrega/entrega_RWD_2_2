30f1876d1231ca08abe2062eb65a1be9ef8a79c5
JosephGabriells <rm92920@fiap.com.br>
2022-10-02 15:41:21 -0300

    PetCadastro Inserido

---
+++ src/Components/PetCadastro/PetCadastro.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { PetFicha } from "../PetFicha/PetFicha";
+import { PetForm } from "../PetForm/PetForm";
+
+import {Container} from "./styled";
+
+
+export const PetCadastro = () =>{
+
+    const [ficha, setFicha] = useState([])
+
+    const addFicha = (petName, petAge,petSize, petBreed, tutorName, tutorPhone, petImage, observations) => {
+        setFicha((prevFicha) => {
+          return [...prevFicha, {name: petName, age: petAge, size: petSize, breed: petBreed, tutorName: tutorName, tutorPhone: tutorPhone, petImage: petImage, observations: observations}];
+        });
+      };
+
+        return (
+        <>
+        
+            <PetForm addFicha={addFicha} />
+            <Container>
+              {ficha.map((note, index) => (
+                <PetFicha 
+                    key={index}
+                    id={index}
+                    name = {note.name}
+                    age = {note.age}
+                    size = {note.size}
+                    breed = {note.breed}
+                    tutorName = {note.tutorName}
+                    tutorPhone = {note.tutorPhone}
+                    petImage = {note.petImage}
+                    observations = {note.observations}  
+                    onDelete={deleteFicha}
+                />
+                ))}
+              </Container>
+        </>
+    )
+}
